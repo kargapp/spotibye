cffb0c275dbc741938223bc005993e537a8ade64
kargapp <122487930+kargapp@users.noreply.github.com>
2024-01-01 14:31:52 -0800

    init shadcn
    WARNING: this build error was thrown when configuring theme.extend.fontFamily in tailwind.config.ts
    
    TypeError: Cannot read properties of undefined (reading 'sans')
    
    Try :root {
        --font-sans: 'Inter', sans-serif;
    }, error magically disappeared even after removing this CSS variable.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,41 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import { cn } from "@/lib/utils";
 
-const font = Inter({ subsets: ["latin"] });
+const font_sans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 export const metadata: Metadata = {
-	title: "Spotibye",
-	description: "Take control of your Spotify library!",
+  title: "Spotibye",
+  description: "Take control of your Spotify library!",
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-	return (
-		<html lang="en">
-			<body className={font.className}>{children}</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body
+        className={cn(
+          "bg-background min-h-screen font-sans antialiased",
+          font_sans.variable,
+        )}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import { ModeToggle } from "@/components/ui/mode-toggle";
+
 export default function Home() {
   return (
     <>
-      <main className="text-green-500">Bye, Spotify!</main>
+      <main className="flex h-screen items-center justify-center">
+        <ModeToggle />
+      </main>
     </>
   );
 }
