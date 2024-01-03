61b82c0e87a038d278430929228be191ceae7642
kargapp <122487930+kargapp@users.noreply.github.com>
2024-01-02 19:35:51 -0800

    skeleton layout
    WARNING: layout not responsive yet.

--- app/components/ui/mode-toggle.tsx
+++ app/components/ui/mode-toggle.tsx
@@ -21,7 +21,9 @@ export function ModeToggle() {
         <Button variant="outline" size="icon">
           <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
+          <span className="sr-only">
+            a button that toggles between light and dark mode
+          </span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
